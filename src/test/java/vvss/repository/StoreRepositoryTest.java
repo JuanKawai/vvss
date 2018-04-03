293a99060c331c0cfb743077c143febcba6743bb
Tamas Nagy <Tamas.Nagy@catalysts.cc>
2018-04-03 14:40:44 +0300

    [VVSS-03] Add dummy test

---
+++ src/test/java/vvss/repository/StoreRepositoryTest.java
@@ -0,0 +1,19 @@
+package vvss.repository;
+import org.junit.*;
+
+/**
+ * Created by tnagy on 4/3/2018.
+ */
+public class StoreRepositoryTest {
+    private StoreRepository target;
+
+    @Before
+    public void setup() {
+        target = new StoreRepository();
+    }
+
+    @Test
+    public void testGetProductsCategory() {
+        assert true;
+    }
+}
